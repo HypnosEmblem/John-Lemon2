223c536e6fdb3a523ccfc8f7ac0cf7cda3bdd1c9
awain1 <an481097@ucf.edu>
2025-12-03 14:49:47 -0500

    mainPush

--- Assets/_3DStealthGame/Scripts/PlayerMovement.cs
+++ Assets/_3DStealthGame/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,6 +11,13 @@ public class PlayerMovement : MonoBehaviour
 
     public float walkSpeed = 1.0f;
     public float turnSpeed = 20f;
+    private bool playerFreeze = false;
+    private int spaceBarPressed = 0;
+
+    // stamina stuff
+    private int stamina = 200;
+    private bool isSprinting = false;
+    public Image circleImage;
 
     Rigidbody m_Rigidbody;
     Vector3 m_Movement;
@@ -24,6 +32,64 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (Random.Range(0,600)==0)
+        {
+            playerFreeze = true;
+            walkSpeed = 0f;
+            turnSpeed = 0f;
+            spaceBarPressed = 0;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            spaceBarPressed++;
+        }
+
+        if (spaceBarPressed > 5)
+        {
+            playerFreeze = false;
+            walkSpeed = 1.0f;
+            turnSpeed = 20f;
+        }
+
+
+        // stamina stuff major mod 
+        //check for shift
+        if (Input.GetKeyDown(KeyCode.LeftShift) && stamina > 50 && !playerFreeze)
+        {
+            isSprinting = true;
+        }
+
+        //check for sprint end early
+        if (Input.GetKeyUp(KeyCode.LeftShift) && !playerFreeze)
+        {
+            walkSpeed = 1.0f;
+            isSprinting = false;
+        }
+        //sprinting loop, drain stam check for low stam
+        if (isSprinting == true && !playerFreeze)
+        {
+            walkSpeed = 2f;
+            stamina--;
+            if (stamina < 0)
+            {
+                isSprinting = false;
+                walkSpeed = 1.0f;
+            }
+        }
+        //regen stam if not sprinting
+        if (isSprinting == false && stamina < 200 && !playerFreeze)
+        {
+            stamina++;
+        }
+
+        //ui circle stuff
+        //find circl scale based off curent stam
+        float scale = Mathf.Clamp(stamina, 0.1f, 200f);
+        scale = scale / 200;
+        //scale circle
+        circleImage.rectTransform.localScale = new Vector3(scale, scale, 1);
+
         var pos = MoveAction.ReadValue<Vector2>();
 
         float horizontal = pos.x;
